de429e483120d5ec705f5ca8e68548779e427824
harkebark <genetafterdark@gmail.com>
2023-08-10 14:56:57 +0000

    Removed problematic imports

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,17 +3,6 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as forScience from './for-science'
-import * as ausPol from './auspol'
-import * as dads from './dads'
-import * as dadsMedia from './dads-media'
-import * as EighteenPlusND from './18-plus-nd'
-import * as ND from './nd'
-import * as discourse from './discourse'
-import * as cats from './cats'
-import * as elusive from './elusive'
-import * as keyboards from './keyboards'
-import * as webhook from './webhook'
 import * as afterdark from './after-dark'
 import { BskyAgent } from '@atproto/api'
 
